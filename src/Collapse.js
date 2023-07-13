7634a42248ed850ed40e975fe9693c297a6ef512
Nerea Fontecha <nerea@landt.co>
2023-07-13 10:49:34 +0200

    Colalpse

--- src/Collapse.js
+++ src/Collapse.js
@@ -2,33 +2,22 @@ import JSUTIL from '@andresclua/jsutil/';
 class Collapse{
     constructor(payload){
         this.jsutil = new JSUTIL();
-        this.element = payload.element
+        this.DOM = {
+            element : payload.element,
+            targetID : payload.element.getAttribute('data-collapse-id'),
+            targetClass : payload.element.getAttribute('data-target-class'),
+            objectClass : payload.element.getAttribute('data-self-class') 
+        }
         this.events();
      
     }
     events(){
         //loop all elements with data-ds-element="collapse"]
             // add event to all of those elements
-            this.element.addEventListener('click', event => {
+            this.DOM.element.addEventListener('click', event => {
                 event.preventDefault(); 
-                var collapseArg = {
-                     /*
-                    * targetID // Collapse element to Expand/Collapse 
-                    * targetClass  // Collapse class applied to Expand/Collapse element || ex: "b--collapse-X--is-active"
-                    */
-                    targetID:this.element.getAttribute('data-collapse-id'),
-                    targetClass : this.element.getAttribute('data-target-class'),
-                    
-                    /*
-                    * objectId // Collapse trigger element  
-                    * objectClass  // Collapse class applied to trigger element || ex: "b--btn-X--is-active"
-                    */
-                    objectId : this.element, 
-                    objectClass : this.element.getAttribute('data-self-class') 
-                    
-                };
                 /* @ NEED TO VALIDATE ARGUMENTS AT SOME POINT */
-                this.collapseEvent(collapseArg)
+                this.collapseEvent()
             })
     }
     /*
@@ -36,18 +25,22 @@ class Collapse{
      * targetClass
      * objectTrigger
      */
-    collapseEvent(payload){
-        
-        if(!document.getElementById(payload.targetID).classList.contains(payload.targetClass)){
-            const getHeight = document.getElementById(payload.targetID).scrollHeight;
-            this.jsutil.addStyle(document.getElementById(payload.targetID),'height',`auto`);
+    collapseEvent(){
+        if(!document.getElementById(this.DOM.targetID).classList.contains(this.DOM.targetClass)){
+            const getHeight = document.getElementById(this.DOM.targetID).scrollHeight;
+            this.jsutil.addStyle(document.getElementById(this.DOM.targetID),'height',`auto`);
         }else{
-            this.jsutil.addStyle(document.getElementById(payload.targetID),'height','0px');
+            this.jsutil.addStyle(document.getElementById(this.DOM.targetID),'height','0px');
         }
         // apply class to b--collapse
-        this.jsutil.toggleClass(document.getElementById(payload.targetID),payload.targetClass)
+        this.jsutil.toggleClass(document.getElementById(this.DOM.targetID),this.DOM.targetClass)
         // apply class to trigger element    
-        this.jsutil.toggleClass(payload.objectId,payload.objectClass)
+        this.jsutil.toggleClass(this.DOM.element,this.DOM.objectClass)
+    }
+    destroy(){
+        this.jsutil.addStyle(document.getElementById(this.DOM.targetID),'height',`auto`);
+        this.jsutil.removeClass(document.getElementById(this.DOM.targetID),this.DOM.targetClass)
+        this.jsutil.removeClass(this.DOM.element,this.DOM.objectClass)
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@
 import Collapse from './Collapse';
 class Page{
     constructor(){
+        this.collapse = [];
         this.init()
     }
     init(){ 
-        document.querySelectorAll('[data-ds-element="collapse"]').forEach(element => {
-            new Collapse({
+        document.querySelectorAll('[data-ds-element="collapse"]').forEach((element, index) => {
+            this.collapse[index] = new Collapse({
                 element : element
             })   
+        });
+        document.querySelector(".js--collapse-destroy").addEventListener("click", (e) => {
+            e.preventDefault()
+            this.destroy()
+        })
+    }
+    destroy(){
+        document.querySelectorAll('[data-ds-element="collapse"]').forEach((element, index) => {
+            this.collapse[index].destroy()  
         });
     }
 }
